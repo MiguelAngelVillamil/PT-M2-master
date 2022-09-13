098e082ba05add73b1f7d5d942e5dde6aec41cf2
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-09-13 14:13:38 -0300

    Homework finalizada

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -10,6 +10,21 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
   // TU CÓDIGO AQUÍ
   
+  // Condición de corte:
+  // Guardamos el match en nuestro array de elementos.
+  if (matchFunc(startEl)) {
+    resultSet.push(startEl);
+  }
+
+  for (let c = 0; c < startEl.children.length; c++) {
+
+    // Cada Elemento tiene un array de hijos con el cuál accedemos desde .children
+    // Para guardar todos los elementos del arbol tenemos que a nuestro array final concatenarle lo que ya tiene con todos los hijos del elemento en el cuál estemos parados y así sucesivamente, o mejor dicho recursivamente.
+    resultSet = [...resultSet, traverseDomAndCollectElements(matchFunc, startEl.children[c])]
+  }
+
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +33,19 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
+
+  switch (selector[0]) {
+
+    case "#":
+      return "id";
   
+    case ".":
+      return "class";
+
+    default:
+
+      return selector.split(".").length > 1 ? "tag.class" : "tag";
+  }
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -27,18 +54,34 @@ var selectorTypeMatcher = function(selector) {
 // matchea el selector.
 
 var matchFunctionMaker = function(selector) {
+
   var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") { 
-   
-  } else if (selectorType === "class") {
+  
+  if (selectorType === "id") { // <div id="id1"></div>
+    return elemento => "#" + elemento.id === selector; // "#" + "id1" === "#id1"
+
+  } else if (selectorType === "class") { // <div class="clase1 clase2"></div>
+    return elemento => {
+
+      let classes = elemento.classList; // ["clase1", "clase2"]
+      
+      for (let i = 0; i < classes.length; i++) {
+        if("." + classes[i] === selector) return true;
+      }
+      return false;
+    }
     
-  } else if (selectorType === "tag.class") {
+  } else if (selectorType === "tag.class") { // <div class="clase1"></div> // div.clase1
+    return elemento => {
+      let [tagOwn, classOwn] = selector.split(".");
+
+      return matchFunctionMaker(tagOwn)(elemento) && matchFunctionMaker("." + classOwn)(elemento);
+    }
     
   } else if (selectorType === "tag") {
+    return elemento => elemento.tagName === selector.toUpperCase();
     
   }
-  return matchFunction;
 };
 
 var $ = function(selector) {
