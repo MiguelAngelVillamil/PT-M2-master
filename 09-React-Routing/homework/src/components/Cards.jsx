7f850a46a5a41886bbdd352ac8a1f1a21bec9711
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-10-07 17:25:16 -0300

    Homework disfuncional

--- 09-React-Routing/homework/src/components/Cards.jsx
+++ 09-React-Routing/homework/src/components/Cards.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import './Cards.css';
-
+import {Link} from 'react-router-dom';
 import Card from './Card.jsx';
 
 export default function Cards({cities, onClose}) {
   return (
+    <Link to='/'>
     <div className='cards'>
+      
+
       {cities.map(c => <Card
           key={c.id}
           max={c.max}
           min={c.min}
           name={c.name}
           img={c.img}
+          id={c.id}
           onClose={() => onClose(c.id)}
-        /> )}
+          /> )}
+      
     </div>
+    </Link>
   );
 }
--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from '../components/About.jsx'
 import Nav from '../components/Nav.jsx';
 import Cards from '../components/Cards.jsx';
+import Ciudad from '../components/Ciudad.jsx'
 
-const apiKey = 'Aqui va la API key que creaste';
+const apiKey = "4ae2636d8dfbdc3044bede63951a019b"
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -46,16 +48,34 @@ function App() {
   }
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-      <div>
-        <Cards
-          cities={cities}
-          onClose={onClose}
+      <Nav onSearch={onSearch} />
+      
+      <Routes>
+        <Route exact path='/'
+          render={() => <Cards cities={cities} onClose={onClose}/>}
         />
-      </div>
-      <hr />
+        <Route exact path='/about'
+          element={<About />}
+        />
+        <Route
+          exact path='/ciudad/:ciudadId'
+          render={({match}) => <Ciudad city={onFilter(match.params.ciudadId)} />}
+        />
+      </Routes>
     </div>
   );
 }
+
+//* Nos falta definir como le pasamos los componentes que queremo renderizar: 
+// 		* a traves de component <Route path="/" component={NavBar} /> en  dicho caso no le podemos 
+// 		pasar props a NavBar
+// 		* a traves de render <Route path="/" render = {() => <NavBar unaProp='hola'/>} /> en esta oportunidad, si
+// 		podemos pasarle props
+// 		* a traves del child method: 
+// 			<Route path="/"> 
+// 				<NavBar unaProp='hola'/> 
+// 			</Route> 
+// 		donde tambien le podemos pasar props y la cantidad de componente que querramos
+
 
 export default App;
