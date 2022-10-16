f81a9737c88b5be8e9d0aa501d8e5cf5b8279da2
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-10-16 01:51:45 -0300

    Homework finalizada

--- 11-Redux/homework/src/components/Counter.js
+++ 11-Redux/homework/src/components/Counter.js
@@ -6,6 +6,8 @@ class Counter extends Component {
     // Extra Credit
     incrementIfOdd = () => {
       //Implementar una función de incremento que sólo aumenta si el valor del contador es impar
+
+      
     };
     // Extra Credit
     incrementAsync = () => {
